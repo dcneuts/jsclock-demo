4a646e6c0265e4fea5dc0feb92f064cbf80547c1
Derek Neuts <dcneuts@gmail.com>
2017-06-22 00:59:57 -0700

    Added logic to the runClock function to prevent CSS from returning suddenly to zero degrees

--- script.js
+++ script.js
@@ -5,20 +5,25 @@ const SECONDHAND = document.querySelector("#second");
 //Added date object, will pull specific methods
 //Setting up variables, will use new Date object for real time
 
+var date = new Date();
+let hr = date.getHours();
+let min = date.getMinutes();
+let sec = date.getSeconds();
+console.log("Hour: " + hr + " Minute: " + min +" Second: " + sec);
+
+//Added calculations for the hands
+
+let hrPosition = (hr*360/12)+(min*(360/60)/12);
+let minPosition = (min*360/60)+(sec*(360/60)/60);
+let secPosition = sec*360/60;
+
 function runClock() {
 
+	//Needed to add to the function to prevent CSS animation rolling back to zero
+	hrPosition = hrPosition+(3/360);
+	minPosition = minPosition+(6/60);
+	secPosition = secPosition+6;
 
-	var date = new Date();
-	let hr = date.getHours();
-	let min = date.getMinutes();
-	let sec = date.getSeconds();
-	console.log("Hour: " + hr + " Minute: " + min +" Second: " + sec);
-
-	//Initial random positions to check functionality
-
-	let hrPosition = (hr*360/12)+(min*(360/60)/12);
-	let minPosition = (min*360/60)+(sec*(360/60)/60);
-	let secPosition = sec*360/60;
 
 	//Pushing the random positions, translating to degrees
 

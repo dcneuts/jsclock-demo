a0b06bcb1d7aaaad930fe97483dfa663d78f617f
Derek Neuts <dcneuts@gmail.com>
2017-06-22 00:40:01 -0700

    Provided basic documentation regarding changes made

--- script.js
+++ script.js
@@ -2,9 +2,13 @@ const HOURHAND = document.querySelector("#hour");
 const MINUTEHAND = document.querySelector("#minute");
 const SECONDHAND = document.querySelector("#second");
 
+//Initial random positions to check functionality
+
 let hrPosition = 30;
 let minPosition = 150;
 let secPosition = 260;
+
+//Pushing the random positions, translating to degrees
 
 HOURHAND.style.transform = "rotate(" + hrPosition + "deg)";
 MINUTEHAND.style.transform = "rotate(" + minPosition + "deg)";

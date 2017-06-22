f671686626dd8049f8dec1b7855d84a13bd2d377
Derek Neuts <dcneuts@gmail.com>
2017-06-22 00:50:23 -0700

    Provided calculations for converting the time from the Date object

--- script.js
+++ script.js
@@ -2,12 +2,6 @@ const HOURHAND = document.querySelector("#hour");
 const MINUTEHAND = document.querySelector("#minute");
 const SECONDHAND = document.querySelector("#second");
 
-//Initial random positions to check functionality
-
-let hrPosition = 30;
-let minPosition = 150;
-let secPosition = 260;
-
 //Added date object, will pull specific methods
 //Setting up variables, will use new Date object for real time
 
@@ -15,6 +9,13 @@ var date = new Date();
 let hr = date.getHours();
 let min = date.getMinutes();
 let sec = date.getSeconds();
+console.log("Hour: " + hr + " Minute: " + min +" Second: " + sec);
+
+//Initial random positions to check functionality
+
+let hrPosition = (hr*360/12)+(min*(360/60)/12);
+let minPosition = (min*360/60)+(sec*(360/60)/60);
+let secPosition = sec*360/60;
 
 //Pushing the random positions, translating to degrees
 

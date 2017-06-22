a8ea38637929cc78b9e93ba097ad7840038f6176
Derek Neuts <dcneuts@gmail.com>
2017-06-22 00:28:46 -0700

    Setup small scope variables for each hand to verify correct elements are selected

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
 const HOURHAND = document.querySelector("#hour");
 const MINUTEHAND = document.querySelector("#minute");
 const SECONDHAND = document.querySelector("#second");
+
+let hrPosition = 30;
+let minPosition = 150;
+let secPosition = 260;
+
+HOURHAND.style.transform = "rotate(" + hrPosition + "deg)";
+MINUTEHAND.style.transform = "rotate(" + minPosition + "deg)";
+SECONDHAND.style.transform = "rotate(" + secPosition + "deg)";

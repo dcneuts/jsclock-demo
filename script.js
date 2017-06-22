0dd9457422eea7ae17a9bba58ef95cfed98d9408
Derek Neuts <dcneuts@gmail.com>
2017-06-22 00:43:48 -0700

    Established new Date object and small scope variables to get time

--- script.js
+++ script.js
@@ -8,6 +8,14 @@ let hrPosition = 30;
 let minPosition = 150;
 let secPosition = 260;
 
+//Added date object, will pull specific methods
+//Setting up variables, will use new Date object for real time
+
+var date = new Date();
+let hr = date.getHours();
+let min = date.getMinutes();
+let sec = date.getSeconds();
+
 //Pushing the random positions, translating to degrees
 
 HOURHAND.style.transform = "rotate(" + hrPosition + "deg)";
